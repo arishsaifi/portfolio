3af1c14d0b9db9c54ed3d8b5dcee1b82330a4336
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-20 10:43:46 +0530

    portifilio boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+const App = () => {
+  return (
+    <main>
+       <div class="p-6 max-w-max h-96 mx-auto my-10 bg-white rounded-xl shadow-lg block items-center space-x-4">
+       
+       <br />
+       <br />
+  <div class="shrink-0 text-3xl text-center font-bold text-cyan-800">
+  Harish saifi
+    
+  </div>
+  <br />
+  <br />
+  <div>
+    <div class="text-xl font-medium text-black">👷‍♂️ website under construction🚧 🏗 ! we are coming soon 😊 </div>
+    <br />
+    <br />
+    <p class="text-slate-700 font-bold">you can explore my 1yeaar old website 🙂</p>
+    <br />
+    <div className="text-center">
+    <a 
+      href="https://arishsaifi.github.io/hs/"
+      alt="HArish"
+
+      >
+        <p className="text-blue-600 font-semibold">
+          Click me for old website🌐
+        </p>
+      </a>
+    </div>
+
+  </div>
+</div>
+    </main>
+   
+  )
+}
+
+export default App
+{/*   
+ 
+ */}
